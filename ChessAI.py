78efc1c803c9e0e15077582ee44b3fcdf9a3e950
WillBox03 <william.t.box@ntlworld.com>
2021-11-02 00:14:12 +0000

    Zobrist hash with  random numbers for squares

--- ChessAI.py
+++ ChessAI.py
@@ -101,85 +101,18 @@ def calculateBoardValue(gs, board):
 
     return boardValue
 
-def findBestestMove(gs, validMoves, returnQueue):
+def findBestMove(gs, validMoves, returnQueue):
     Depth = 4
-    CurrentDepth = 0
     Max = -CHECKMATE
     Min = CHECKMATE
-
-    '''if gs.whiteToMove:
-        returnQueue.put(minimax(gs, Depth, True)[1])
-    else:
-        returnQueue.put(minimax(gs, Depth, False)[1])'''
-
-    '''if gs.whiteToMove:
-        returnQueue.put(minimaxabp(gs, Depth, True, Max, Min)[1])
-    else:
-        returnQueue.put(minimaxabp(gs, Depth, False, Max, Min)[1])'''
 
     if gs.whiteToMove:
         returnQueue.put(minimaxabpmo(gs, Depth, True, Max, Min)[1])
     else:
         returnQueue.put(minimaxabpmo(gs, Depth, False, Max, Min)[1])
 
-    '''if gs.whiteToMove:
-        returnQueue.put(maxestValue(gs, Depth, CurrentDepth, Max, Min)[1])
-    else:
-        returnQueue.put(minestValue(gs, Depth, CurrentDepth, Max, Min)[1])'''
-
-def minimax(gs, depth, maximizing):
-    if depth == 0:
-        return calculateBoardValue(gs, gs.board), None
-
-    move = None
-    highestValue = -CHECKMATE if maximizing else CHECKMATE
-    moves = gs.getValidMoves()
-    for currentMove in moves:
-        gs.makeMove(currentMove)
-        if gs.checkmate:
-            tempValue = CHECKMATE if maximizing else -CHECKMATE
-            return tempValue, currentMove
-        elif gs.stalemate:
-            tempValue = STALEMATE
-            return tempValue, currentMove
-        else:
-            tempValue = minimax(gs, depth - 1, not maximizing)[0]
-        if (tempValue > highestValue and maximizing) or (tempValue < highestValue and not maximizing):
-            highestValue = tempValue
-            move = currentMove
-        gs.undoMove()
-    return highestValue, move
-
-def minimaxabp(gs, depth, maximizing, alpha, beta):
-    if depth == 0:
-        return calculateBoardValue(gs, gs.board), None
-
-    move = None
-    highestValue = -CHECKMATE if maximizing else CHECKMATE
-    moves = gs.getValidMoves()
-    for currentMove in moves:
-        gs.makeMove(currentMove)
-        if gs.checkmate:
-            tempValue = CHECKMATE if maximizing else -CHECKMATE
-            return tempValue, currentMove
-        elif gs.stalemate:
-            tempValue = STALEMATE
-            return tempValue, currentMove
-        else:
-            tempValue = minimaxabp(gs, depth - 1, not maximizing, alpha, beta)[0]
-            if maximizing:
-                alpha = max(alpha, tempValue)
-            else:
-                beta = min(beta, tempValue)
-        if (tempValue > highestValue and maximizing) or (tempValue < highestValue and not maximizing):
-            highestValue = tempValue
-            move = currentMove
-        gs.undoMove()
-        if alpha >= beta:
-            break
-    return highestValue, move
-
 def minimaxabpmo(gs, depth, maximizing, alpha, beta):
+
     if depth == 0:
         return calculateBoardValue(gs, gs.board), None
 
@@ -223,69 +156,3 @@ def orderMoves(moves):
                 moveScores[k], moveScores[swapIndex] = moveScores[swapIndex], moveScores[k]
     #print(moveScores)
     return moves
-
-
-
-
-def maxestValue(gs, Depth, CurrentDepth, Max, Min):
-    if Depth != CurrentDepth:
-        CurrentDepth += 1
-        move = None
-        highestValue = -CHECKMATE
-        moves = orderMoves(gs.getValidMoves())
-        for currentMove in moves:
-            gs.makeMove(currentMove)
-            #print(gs.board)
-            if gs.checkmate:
-                tempValue = CHECKMATE
-                return tempValue, currentMove
-            elif gs.stalemate:
-                tempValue = STALEMATE
-                return tempValue, currentMove
-            else:
-                tempValue = minestValue(gs, Depth, CurrentDepth, Max, Min)[0]
-                Max = max(Max, tempValue)
-            if tempValue > highestValue:
-                highestValue = tempValue
-                move = currentMove
-            gs.undoMove()
-            if Max >= Min:
-                break
-        #print(highestValue)
-        return highestValue, move
-    else:
-        boardValue = calculateBoardValue(gs, gs.board)
-        #print(boardValue)
-        return boardValue, None
-
-
-def minestValue(gs, Depth, CurrentDepth, Max, Min):
-    if Depth != CurrentDepth:
-        CurrentDepth += 1
-        move = None
-        highestValue = CHECKMATE
-        moves = orderMoves(gs.getValidMoves())
-        for currentMove in moves:
-            gs.makeMove(currentMove)
-            #print(gs.board)
-            if gs.checkmate:
-                tempValue = -CHECKMATE
-                return tempValue, currentMove
-            elif gs.stalemate:
-                tempValue = STALEMATE
-                return tempValue, currentMove
-            else:
-                tempValue = maxestValue(gs, Depth, CurrentDepth, Max, Min)[0]
-                Min = min(Min, tempValue)
-            if tempValue < highestValue:
-                highestValue = tempValue
-                move = currentMove
-            gs.undoMove()
-            if Max >= Min:
-                break
-        #print(highestValue)
-        return highestValue, move
-    else:
-        boardValue = calculateBoardValue(gs, gs.board)
-        #print(boardValue)
-        return boardValue, None
---
+++ ZobristHashing.py
@@ -0,0 +1,99 @@
+import numpy as np
+import os.path
+
+PieceTypeIndexConversion = {"K": 0, "p" : 1, "N" : 2, "B" : 3, "R" : 4, "Q" : 5}
+
+class ZobristHash:
+
+    def __init__(self):
+        
+        self.seed = np.random.seed(2361912)
+        self.randomNumbersFileName = "RandomNumbers.txt"
+        self.piecesArray = np.empty([6, 2, 64], dtype = np.uint64)
+        self.castlingRights = np.empty([16], dtype = np.uint64)
+        self.enPassantFile = np.empty([8], dtype = np.uint64)
+        self.sideToMove = np.uint64(0)
+        
+
+    def writeRandomNumbers(self):
+        self.randomNumberString = ""
+        self.numRandomNumbers = (64 * 6 * 2) + 16 + 8 + 1
+
+        for i in range(self.numRandomNumbers):
+            i += 1
+            self.randomNumberString += str(self.randomUnsigned64BitInteger())
+            if i != self.numRandomNumbers:
+                self.randomNumberString += ","
+
+            with open(self.randomNumbersFileName, "w") as file:
+                file.write(self.randomNumberString)
+
+    def readRandomNumbers(self):
+        if not os.path.isfile(self.randomNumbersFileName):
+            print("Creating File and Numbers...")
+            self.writeRandomNumbers()
+            print("Done!")
+
+        with open(self.randomNumbersFileName, "r") as file:
+            self.randomNumbers = file.read()
+        
+        self.randomNumbers = self.randomNumbers.split(",")
+            
+        return self.randomNumbers
+
+    
+    def Zobrist(self):
+        
+        self.ZobristNumbers = self.readRandomNumbers()
+
+        for square in range(64):
+            for pieceIndex in range(6):
+                self.piecesArray[pieceIndex, 0, square] = self.ZobristNumbers.pop()
+                self.piecesArray[pieceIndex, 1, square] = self.ZobristNumbers.pop()
+
+        for right in range(16):
+            self.castlingRights[right] = self.ZobristNumbers.pop()
+            
+        for file in range(8):
+            self.enPassantFile[file] = self.ZobristNumbers.pop()       
+
+        self.sideToMove = np.uint64(self.ZobristNumbers.pop())
+
+    def CalculateZobristKey(self, gs):
+
+        self.ZobristKey = np.uint64(0)
+        self.board = gs.board
+        self.squareCount = 0
+
+        for rank in range(len(self.board)):
+            for file in range(len(self.board)):
+                if self.board[rank][file] != "--":
+                    self.pieceType = PieceTypeIndexConversion[self.board[rank][file][1]]
+                    self.pieceColour = 0 if self.board[rank][file][0] == "w" else 1
+
+                    self.ZobristKey = np.bitwise_xor(self.ZobristKey, self.piecesArray[self.pieceType, self.pieceColour, self.squareCount])
+                
+                self.squareCount += 1
+        
+        self.enPassantIndex = gs.enPassantPossible
+        if self.enPassantIndex != ():
+            self.ZobristKey = np.bitwise_xor(self.ZobristKey, self.enPassantFile[self.enPassantIndex[1]])
+            
+        self.whiteToMove = gs.whiteToMove
+        if self.whiteToMove:
+            self.ZobristKey = np.bitwise_xor(self.ZobristKey, self.sideToMove)
+
+        self.listOfCastlingRights = gs.currentCastlingRight
+        self.castleRightsIndex = 0
+        self.castleRightsIndex += 1 if self.listOfCastlingRights.wks else 0 
+        self.castleRightsIndex += 2 if self.listOfCastlingRights.bks else 0 
+        self.castleRightsIndex += 4 if self.listOfCastlingRights.wqs else 0 
+        self.castleRightsIndex += 8 if self.listOfCastlingRights.bqs else 0 
+
+        self.ZobristKey = np.bitwise_xor(self.ZobristKey,self.castlingRights[self.castleRightsIndex])
+
+        return np.binary_repr(self.ZobristKey)
+
+    def randomUnsigned64BitInteger(self):
+        self.random64BitInteger = np.random.randint(0, 18446744073709551615, dtype = np.uint64)
+        return self.random64BitInteger
